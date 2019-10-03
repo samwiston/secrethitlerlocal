436ccc5ac578d7f300faf14918c0160346980b66
Nicholas VanDette <nicholas.vandette@nuix.com>
2019-10-03 15:52:58 -0400

    wip

--- jbsh-gameserver/new/player-device/src/App.js
+++ jbsh-gameserver/new/player-device/src/App.js
@@ -1,10 +1,48 @@
 import React from 'react';
-import logo from './logo.svg';
+import io from 'socket.io-client'
+let socket = io(`http://localhost:8080`)
 
-function App() {
-  return (
-    <input type="text" name="user"/>
-  );
+class App extends React.Component {
+    // TODO: make text input / submit component
+    state = {
+        data: {},
+        nameInput: ""
+    };
+
+    componentDidMount() {    
+        socket.on('server:event', data => {
+            this.setState({ data })
+        })
+    }
+    
+    emit = (type, message) => {
+        socket.emit(type, message)
+    }
+
+    sendUsername = this.sendUsername.bind(this);
+    sendUsername(e) {
+        this.emit('username', e.target.value);
+    }
+
+    handleChange = this.handleChange.bind(this);
+    handleChange(e) {
+        this.setState({nameInput: e.target.value})
+    }
+
+    render() {
+        return (
+            <div>
+                <input 
+                    id="txt" 
+                    autocomplete="off" 
+                    autofocus="on" 
+                    placeholder="type your name here..." 
+                    onChange={this.handleChange}
+                    />
+                <button onClick={this.sendUsername}>Send</button>
+            </div>
+        )
+    }
 }
 
 export default App;
